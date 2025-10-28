a0c9562e7e72ddb8225120d53e1d69e418da23d3
koudagawa <koudagawa@icloud.com>
2025-10-28 20:21:30 +0900

    Initial Commit

---
+++ FukuwaraiDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FukuwaraiDemo
+//
+//  Created by 宇田川航太 on 2025/10/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FukuwaraiDemo/FukuwaraiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FukuwaraiDemoApp.swift
+//  FukuwaraiDemo
+//
+//  Created by 宇田川航太 on 2025/10/28.
+//
+
+import SwiftUI
+
+@main
+struct FukuwaraiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
